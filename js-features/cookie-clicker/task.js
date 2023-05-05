e1f3857d761a010fc47cfb0c20979457f9609055
hitman46923 <al46923@outlook.com>
2023-05-06 00:13:31 +0300

    complete features

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,36 +1,31 @@
 
-let clicker = document.getElementById('clicker__counter');
-
-let cookie = document.getElementById('cookie');
-
-let plug = true; /* заглушка чтобы меять стили */
+  let clicker = document.getElementById('clicker__counter');
+  let cookie = document.getElementById('cookie');
+  let plug = true; /* заглушка чтобы меять стили */
 
 
 
-cookie.onclick = function counterStart () {
-    clicker.textContent ++ ;
+  cookie.onclick = function counterStart() {
+  	clicker.textContent++;
 
-    cookie.style.width = '200px';
+  	cookie.style.width = '200px';
 
-    if (plug) {
-        cookie.style.width = '100px';
-        cookie.style.height = '100px';
-        plug = false;
-    } else {
-        cookie.style.width = '200px';
-        cookie.style.height = '200px';
-        plug = true;
-
-
-    }
-   
+  	if (plug) {
+  		cookie.style.width = '100px';
+  		cookie.style.height = '100px';
+  		plug = false;
+  	} else {
+  		cookie.style.width = '200px';
+  		cookie.style.height = '200px';
+  		plug = true;
 
 
-
-}
+  	}
 
 
 
+
+  }
 
 
 
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -7,10 +7,10 @@ let deadsCounter = 0;
 
 
 
-function clearAll () {
-   deads.textContent = 0;
-    losts.textContent = 0;
-    
+function clearAll() {
+	deads.textContent = 0;
+	losts.textContent = 0;
+
 }
 
 
@@ -18,51 +18,45 @@ function clearAll () {
 
 
 function getHole(holeIndex) {
-    return document.getElementById("hole" + holeIndex.toString());
-  }
-  
+	return document.getElementById("hole" + holeIndex.toString());
+}
 
 
 
- 
 
 
-   for (let i = 1; i <= 9; i++) {
-        let elem = getHole(i);
 
 
-        elem.onclick = () =>  {
-            if (elem.classList.contains('hole_has-mole') ) {
-                deadsCounter = deadsCounter + 1;
-                deads.textContent = deadsCounter;
-             if (deadsCounter === 10) {
-                alert('Победа')
-                clearAll ();
-             }   
-                
-    
-        } else  {
-            lostsCounter = lostsCounter + 1;
-            losts.textContent = lostsCounter;
-            if (lostsCounter === 5) {
-                alert('Поражение');
-                clearAll();
-            }     
-       
-      
-        }
-
- 
-    
-    
-     }
-    
-     
-
-        }
-     
+for (let i = 1; i <= 9; i++) {
+	let elem = getHole(i);
 
 
- 
- 
+	elem.onclick = () => {
+		if (elem.classList.contains('hole_has-mole')) {
+			deadsCounter = deadsCounter + 1;
+			deads.textContent = deadsCounter;
+			if (deadsCounter === 10) {
+				alert('Победа')
+				clearAll();
+			}
 
+
+		} else {
+			lostsCounter = lostsCounter + 1;
+			losts.textContent = lostsCounter;
+			if (lostsCounter === 5) {
+				alert('Поражение');
+				clearAll();
+			}
+
+
+		}
+
+
+
+
+	}
+
+
+
+}
