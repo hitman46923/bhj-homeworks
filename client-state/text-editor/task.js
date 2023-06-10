c56f096da191088580d351a313d715a4c61628d0
hitman46923 <al46923@outlook.com>
2023-06-10 13:29:14 +0300

    fix

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,3 +1,20 @@
-let editor = document.getElementById('editor');
+function saveText() {
+    let editor = document.getElementById("editor");
+    let text = editor.value;
+    localStorage.setItem("savedText", text);
+}
 
-localStorage.setItem(editor);
+
+function restoreText() {
+    let editor = document.getElementById("editor");
+    let savedText = localStorage.getItem("savedText");
+    if (savedText) {
+        editor.value = savedText;
+    }
+}
+
+window.onload = function() {
+    restoreText(); 
+    let editor = document.getElementById("editor");
+    editor.onkeyup = saveText; 
+};
